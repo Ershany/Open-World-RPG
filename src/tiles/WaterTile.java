0d4e9644fda6a2722961a6381c32e8f41fb47c51
Ershany <bradyjessup@hotmail.com>
2015-03-04 23:22:42 -0500

    Water Animation Speed Hotfix
    Slowed down the water animation speed

--- src/tiles/WaterTile.java
+++ src/tiles/WaterTile.java
@@ -15,16 +15,16 @@ public class WaterTile extends Tile {
 
 	public void update() {
 		anim++;
-		if(anim % 180 == 0) {
+		if(anim % 720 == 0) {
 			tileImage = Sprite.water1.getImage();
 			
 			//reset anim
 			anim = 0;
 		}
-		else if(anim % 120 == 0) {
+		else if(anim % 480 == 0) {
 			tileImage = Sprite.water2.getImage();
 		}
-		else if(anim % 60 == 0) {
+		else if(anim % 240 == 0) {
 			tileImage = Sprite.water3.getImage();
 		}
 	}
