622d9a4bc851929226ad832fb7b0207bf4e7fd3a
Kevin Corina <Kevin.p.corina@gmail.com>
2015-03-17 17:41:00 -0400

    Hello! Trying to gain some momentum again. Overhauled some of your move code for efficency, clarity, and cleanliness. Feel free to check to change it back if you want, I kept your code commented in.

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -21,7 +21,9 @@ public class Player extends Mob {
 	private boolean upHeld, downHeld, rightHeld, leftHeld;
 	
 	//corners  x2/y2 top right, x3/y3 bottom left, x4/y4 bottom right, x5/y5 middle left, x6/y6 middle right
-	private float x2, x3, x4, x5, x6, y2, y3, y4, y5, y6;
+	//private float x2, x3, x4, x5, x6, y2, y3, y4, y5, y6;
+	private float[] xVals, yVals;
+	
 	
 	public Player(int x, int y, int level, LevelState state, Tilemap tilemap) {
 		super(x, y, level, state, tilemap);
@@ -29,6 +31,8 @@ public class Player extends Mob {
 
 	@Override
 	public void init() {
+		
+		
 		health = level * 5; //it should be:    health = (level * 5) + inventory.getArmor();
 		currentHealth = health;
 		damage = level * 2;  //it should be:   damage = (level * 2) + inventory.getWeapon().getDamage();
@@ -37,11 +41,20 @@ public class Player extends Mob {
 		height = 48;
 		
 		hitbox = new Rectangle(width, height);
-		x2 = x + width; y2 = y;
-		x3 = x;         y3 = y + height;
-		x4 = x + width; y4 = y + height;
-		x5 = x;         y5 = y + (height / 2);
-		x6 = x + width; y6 = y + (height / 2);
+		
+		xVals = new float[5];
+		yVals = new float[5];
+		
+		for (int i = 0; i<= xVals.length-1; i++) {
+			xVals [i] = x + width*((i+1)%2);			
+		}
+		System.out.println("new player created");
+		
+		yVals [0] = y;
+		yVals [1] = y + height;
+		yVals [2] = y + height;
+		yVals [3] = y + (height / 2);
+		yVals [4] = y + (height / 2);
 	}
 
 	@Override	
@@ -62,14 +75,57 @@ public class Player extends Mob {
 		g.fillRect((int)x - currentTilemap.getXOffset(), (int)y - currentTilemap.getYOffset(), width, height);
 	}
 	
+	private boolean move (float testSpeedX, float testSpeedY) {
+		if (currentTilemap.getTile((int) (x +  testSpeedX), (int)(y + testSpeedY)).getSolid()) {
+			return false;
+		}
+		for (int i = 0; i<= xVals.length-1; i++) {
+			if (currentTilemap.getTile((int) (xVals[i] +  testSpeedX), (int)(yVals[i] + testSpeedY)).getSolid()) {
+				return false;
+			}
+		}
+
+		x += testSpeedX;
+		y += testSpeedY;
+		for (int i = 0; i<= xVals.length-1; i++) {
+				xVals[i]  += testSpeedX;
+				yVals[i]  += testSpeedY;	
+		}
+		
+		
+		return true;
+	}
+	
 	private void checkMovement() {
+		//TODO : Fix diag
+		
+		
+		if (upHeld) {
+			move(0,-speed);
+		}
+		if (downHeld) {
+			move(0,speed);
+		}
+		if (leftHeld) {
+			move(-speed,0);
+		}
+		if (rightHeld) {
+			move(speed,0);
+			
+		}
+		
+	}
+		/*int xMod = 0;
+		int yMod = 0;
 		if(upHeld) {
 			if(!currentTilemap.getTile((int)x, (int)(y - speed)).getSolid() &&
 			!currentTilemap.getTile((int)x2, (int)(y2 - speed)).getSolid() &&
 			!currentTilemap.getTile((int)x3, (int)(y3 - speed)).getSolid() &&
 			!currentTilemap.getTile((int)x4, (int)(y4 - speed)).getSolid() &&
 			!currentTilemap.getTile((int)x5, (int)(y5 - speed)).getSolid() && 
-			!currentTilemap.getTile((int)x6, (int)(y6 - speed)).getSolid()) {
+			!currentTilemap.getTile((int)x6, (int)(y6 - speed)).getSolid()) 
+			
+			{
 				y -= speed;
 				y2 -= speed;
 				y3 -= speed;
@@ -122,8 +178,7 @@ public class Player extends Mob {
 				x5 += speed;
 				x6 += speed;
 			}
-		}
-	}
+		}*/
 	
 	private void updateOffset() {
 		currentTilemap.setXOffset((int)x - (Game.WIDTH / 2) + (width / 2));
